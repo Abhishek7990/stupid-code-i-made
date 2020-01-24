dbd9f90046cd8f124c4c9c90ead05578d80db635
Calamity <59738161+Calamity34@users.noreply.github.com>
2020-01-24 21:28:11 +0500

    Added lots of actions

--- 2InputCalculator.py
+++ 2InputCalculator.py
@@ -1,5 +1,21 @@
 a = input("Enter the first number: ")
+action = input("Choose an action (add/subtract/divide/multiply/expotent): ")
 b = input("Enter the second number: ")
-print("You entered this:", a, b)
-sum = int(a) + int(b)
-print("Your output:", sum)
+
+if action == "add":
+    equals = int(a) + int(b)
+    print("Your output:", equals)
+elif action == "subtract":
+    equals = int(a) - int(b)
+    print("Your output:", equals)
+elif action == "divide":
+    equals = int(a) / int(b)
+    print("Your output:", equals)
+elif action == "multiply":
+    equals = int(a) * int(b)
+    print("Your output:", equals)
+elif action == "expotent":
+    equals = int(a) ** int(b)
+    print("Your output:", equals)
+elif action is not "add" or "subtract" or "divide" or "multiply" or "expotent":
+    print("Incorrect action!")
