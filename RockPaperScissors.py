073def1b4a52b57adc4702134b2753e91b35852f
Calamity <NikitaYoutube2008@icloud.com>
2020-01-25 16:09:44 +0500

    Added dictionary
    Thanks to watch?v=Uj1ykZWtPYI#0601 in Python Discord Server

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,18 +1,23 @@
 import random
 
-types = ["Rock", "Paper", "Scissors"]
-AI = random.choice(types)
+types = {"Scissors": "Paper", "Paper": "Rock", "Rock": "Scissors"}
+AI = random.choice(list(types.keys()))
 
 user = input("\nChoose 'Rock', 'Paper', or 'Scissors': ")
 print("\nYou chose:", user)
 print("The AI chose:", AI)
 
 # If user ties:
-if AI == "Rock" and user == "Rock" or AI == "Paper" and user == "Paper" or AI == "Scissors" and user == "Scissors":
+if AI == user:
     print("\nTie!")
-# If user wins:
-elif AI == "Rock" and user == "Paper" or AI == "Paper" and user == "Scissors" or AI == "Scissors" and user == "Rock":
-    print("\nYou won!")
 # If user loses:
-elif AI == "Rock" and user == "Scissors" or AI == "Paper" and user == "Rock" or AI == "Scissors" and user == "Paper":
+elif types.get(AI) == user:
     print("\nYou lose!")
+# If user wins:
+else:
+    print("\nYou won!")
+# =========================================================+
+# The inspiration: someone in Xisumavoid's livestream chat |
+# Main coding: Calamity34                                  |
+# Dictionary: watch?v=Uj1ykZWtPYI#0601 at Python Discord   |
+# =========================================================+
