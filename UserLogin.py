e9e33ff172259123ddefa58566160abd17741fa3
Calamity <59738161+Calamity34@users.noreply.github.com>
2020-01-25 11:48:04 +0500

    a simple user login concept

--- UserLogin.py
+++ UserLogin.py
@@ -3,4 +3,4 @@ password = input("User password: ")
 if login == "admin" and password == "root" or login == "Calamity34" and password == "FoxBot":
     print("Login Success!")
 else:
-    print("Wrong password!")
+    print("Wrong password!") 
