8f839d9c8616374ac05724a20ec2d034a246ab12
Calamity <NikitaYoutube2008@icloud.com>
2020-01-25 18:46:08 +0500

    remind me to finish this tomorrow
    h

---
+++ 8InputCalculator.py
@@ -0,0 +1,16 @@
+a = input("First number: ")
+x = input("Action (add/subtract/divide/multiply): ")
+b = input("Second number: ")
+question = input("Any more numbers? (yes/no)")
+
+if x == "add":
+    y = "+"
+
+geralt = int(a) + int(b)
+
+if question == "no":
+    print("Output:", geralt) 
+elif question == "yes":   
+    print("test")
+
+# i am tired and going to finish this tomorrow
