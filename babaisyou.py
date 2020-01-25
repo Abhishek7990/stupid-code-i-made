fb972c190bb0c4e7fb52f10476ddfa04e5c5f070
Calamity <59738161+Calamity34@users.noreply.github.com>
2020-01-25 12:15:32 +0500

    baba is you (atari edition)

---
+++ babaisyou.py
@@ -0,0 +1,11 @@
+def baba():
+    print("baba is you \n is \nwin")
+
+print("how to win:")
+win = input("Type 'baba' to win: ")
+if win == "baba":
+    baba()
+    print("     ")
+    print("Congratulation")
+else:
+    print("Baba is not you!  Press 'Run' to restart."
