d2c0b27078a11825dcfec3cb2f8c1930efc90244
Calamity <NikitaYoutube2008@icloud.com>
2020-01-30 18:40:50 +0500

    Fixed all errors and made some customs
    Thx to .Yn#2512 at Python Discord server for helping me with exceptions!

--- DollarToRuble.py
+++ DollarToRuble.py
@@ -7,23 +7,31 @@ way = input("Dollar to Ruble or backwards? (to R; to D): ")
 
 # The money counting:
 if way == "to R":
-    amount = input("\nInput the amount of dollars: ")
-    if float(amount) >= 1:
-        answer = float(amount) * 62
+    try:
+        amount = float(input("\nInput the amount of dollars: "))
+        if amount < 0.01:
+            raise ValueError
+    except ValueError:
+            print("\nNot a Number or is null!")
     else:
-        print("Incorrect!")
-    
+        answer = amount * 62
 elif way == "to D":
-    amount = input("\nInput the amount of rubles: ")
-    if float(amount) >= 1:
-        answer = float(amount) / 62
+    try:
+        amount = input("\nInput the amount of rubles: ")
+        if amount < 0.01:
+            raise ValueError
+    except ValueError:
+        print("\nNot a Number or is null!")
     else:
-        print("Incorrect")
+        answer = amount / 62
 else:
     print("\nIncorrect money type!")
 
 # The result:
-print("\nThe amount of money is: %f" % answer)
+try:
+    print("\nThe amount of money is: %f" % answer)
+except NameError:
+    print("\nTo restart the stonk exchange - run the code again.")
 time.sleep(0.4)
 print("\nThanks for using Stonk Exchange! Cya!")
 time.sleep(0.5)
