6466eedb96aea042f59fb27a816e6f3c06437296
Calamity <NikitaYoutube2008@icloud.com>
2020-01-31 16:54:04 +0500

    Added language support

--- CeasarEncoder.py
+++ CeasarEncoder.py
@@ -1,17 +1,31 @@
+lang = input("'Russian' or 'English'? ")
+
 # we need 2 helper mappings, from letters to ints and the inverse
-L2I = dict(zip("ABCDEFGHIJKLMNOPQRSTUVWXYZ",range(26)))
-I2L = dict(zip(range(26),"ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+if lang == "Russian":
+    L2I = dict(zip("АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЭЮЯ",range(33)))
+    I2L = dict(zip(range(33),"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЭЮЯ"))
+
+    plaintext = input("Message to encrypt: ")
+elif lang == "English":
+    L2I = dict(zip("ABCDEFGHIJKLMNOMPQRSTUVWXYZ",range(26)))
+    I2L = dict(zip(range(26),"ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+
+    plaintext = input("Message to encrypt: ")
+else:
+    print("Not an option.")
 
 key = 3
-plaintext = input("Message to encrypt: ")
 
 # encipher
-ciphertext = ""
-for c in plaintext.upper():
-    if c.isalpha(): ciphertext += I2L[ (L2I[c] + key)%26 ]
-    else: ciphertext += c
+try:
+    ciphertext = ""
+    for c in plaintext.upper():
+        if c.isalpha(): ciphertext += I2L[ (L2I[c] + key)%26 ]
+        else: ciphertext += c
 
-print(f"Your message: {ciphertext}")
+    print(f"Your message: {ciphertext}")
+except NameError:
+    this = "is a blank line"
 
 # ok this is not actually mine, all made by:
 # https://gist.github.com/jameslyons/8701593
