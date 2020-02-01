7362d918c84f53cb6ae615bab586a70f96e97ae3
Calamity <NikitaYoutube2008@icloud.com>
2020-02-02 00:02:35 +0500

    smol boi
    whats the weight

--- RandomHexColor.py
+++ RandomHexColor.py
@@ -1,11 +1,1 @@
-import random
-
-list = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"]
-a = random.choice(list)
-b = random.choice(list)
-c = random.choice(list)
-d = random.choice(list)
-e = random.choice(list)
-f = random.choice(list)
-
-print(f"Your color: #{a}{b}{c}{d}{e}{f}")
+import random;l = list("0123456789abcdef");print("Your color: #","".join(random.choices(l, k=6)))
