7bb1232e2dd4f200f6c6a5a6436e6df9962730da
Calamity <NikitaYoutube2008@icloud.com>
2020-02-01 15:42:37 +0500

    Random hex color generator

---
+++ RandomHexColor.py
@@ -0,0 +1,11 @@
+import random
+
+list = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"]
+a = random.choice(list)
+b = random.choice(list)
+c = random.choice(list)
+d = random.choice(list)
+e = random.choice(list)
+f = random.choice(list)
+
+print(f"Your color: #{a}{b}{c}{d}{e}{f}")
