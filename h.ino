493cb3e21f0020b62849c7d0fc58cfd7a1aad346
Calamity <59738161+Calamity34@users.noreply.github.com>
2020-03-02 13:03:29 +0500

    h

---
+++ h.ino
@@ -0,0 +1,31 @@
+#define h delay // 1
+#define hh void // 2
+#define hhh setup // 3
+#define hhhh pinMode // 4
+#define hhhhh OUTPUT // 5
+#define hhhhhh loop // 6
+#define hhhhhhh analogWrite // 7
+#define hhhhhhhh 9 // 8
+#define hhhhhhhhh 85 // 9
+#define hhhhhhhhhh 250 // 10
+#define hhhhhhhhhhh 170 // 11
+#define hhhhhhhhhhhh 255 // 12
+#define hhhhhhhhhhhhh 11 // 13
+
+
+hh hhh(){
+    hhhh(hhhhhhhh,hhhhh);
+    hhhh(hhhhhhhhhhhhh,hhhhh);
+}
+
+hh hhhhhh(){
+    hhhhhhh(hhhhhhhh,hhhhhhhhh);
+    hhhhhhh(hhhhhhhhhhhhh,hhhhhhhhhhhh);
+    h(hhhhhhhhhh);
+    hhhhhhh(hhhhhhhh,hhhhhhhhhhh);
+    hhhhhhh(hhhhhhhhhhhhh,hhhhhhhhhhh);
+    h(hhhhhhhhhh);
+    hhhhhhh(hhhhhhhhh,hhhhhhhhhhhh);
+    hhhhhhh(hhhhhhhhhhhhh,hhhhhhhhh);
+    h(hhhhhhhhhh);
+}
